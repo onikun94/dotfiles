cfe9d0526f144c34356a51b831f5bd45ec13da3c
onikun94 <onikun94@gmail.com>
2022-08-24 00:47:12 +0900

    😆 first commit

---
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,16 @@
+-- Just an example, supposed to be placed in /lua/custom/
+
+local M = {}
+
+-- make sure you maintain the structure of `core/default_config.lua` here,
+-- example of changing theme:
+M.plugins = {
+  user = require "custom.plugins",
+  override = require "custom.override",
+}
+
+M.ui = {
+  theme = "gatekeeper",
+}
+
+return M
---
+++ dot_config/nvim/lua/custom/override.lua
@@ -0,0 +1,28 @@
+return {
+  ["williamboman/mason.nvim"] = {
+    -- :Mason
+    -- https://github.com/williamboman/mason.nvim/blob/main/PACKAGES.md
+    ensure_installed = {
+      -- lua
+      "lua-language-server",
+      "stylua",
+
+    },
+  },
+
+ ["NvChad/ui"] = {
+    --NvChad-after = "base46",
+    module = "nvchad_ui",
+    statusline = {
+      separator_style = "block",
+      overriden_modules = nil,
+    },
+    tabufline = {
+      lazyload = false, -- to show tabufline by default
+   },
+    config = function()
+      require("plugins.configs.others").nvchad_ui()
+    end,
+  },
+
+}
