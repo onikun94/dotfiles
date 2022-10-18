e547578a294731f3defa8a9edd4aefb6790a7a04
onikun94 <onikun94@gmail.com>
2022-10-18 23:55:01 +0900

    suggest def

--- dot_config/nvim/lua/custom/plugins/lspconfig.lua
+++ dot_config/nvim/lua/custom/plugins/lspconfig.lua
@@ -4,10 +4,19 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require ("lspconfig")
 local util = require("lspconfig/util")
 
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>m', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
 on_attach = function (client,bufnr)
   local bufopts = {noremap=true,silent=true, buffer=bufnr}
+  vim.keymap.set('n', 'gk', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n','gd',vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gd<Space>', ':split | lua vim.lsp.buf.definition()<CR>', bufopts)
+  vim.keymap.set('n', 'gd<CR>', ':vsplit | lua vim.lsp.buf.definition()<CR>', bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
 -- :h lspconfig-all
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
