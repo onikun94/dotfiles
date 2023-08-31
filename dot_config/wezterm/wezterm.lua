b2426c6d7f803cb858f8de51da9da38614aeca7e
onikun94 <onikun94@gmail.com>
2023-08-31 23:34:55 +0900

    chore: comment out enable_tab_bar

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -12,7 +12,7 @@ return {
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
   --my setting
-  enable_tab_bar = false,
+  --enable_tab_bar = false,
   window_decorations = "RESIZE",
   leader = { key = "t", mods = "CTRL", timeout_milliseconds = 1000 },
   keys = {
