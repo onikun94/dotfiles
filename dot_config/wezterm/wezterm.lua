483bca38088b8ca9cf5fd2dbd52d80a969a7fc49
onikun94 <onikun94@gmail.com>
2024-09-14 11:19:10 +0900

    chore: wezterm 修正

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,18 +2,13 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 require("events.tabtitle").setup()
 return {
-  window_background_opacity = 0.95,
-  --font = wezterm.font("Cica"), -- 自分の好きなフォントいれる
   font = wezterm.font("Hack Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" }),
-  use_ime = true, -- wezは日本人じゃないのでこれがないとIME動かない
+  use_ime = true,
   initial_cols = 260,
   initial_rows = 65,
   font_size = 16.0,
-  --color_scheme = "OneHalfDark", -- 自分の好きなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
-  --my setting
-  --enable_tab_bar = false,
   window_decorations = "RESIZE",
   colors = {
     foreground = "#9ea3c0",
@@ -36,10 +31,8 @@ return {
     { key = "J",        mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
     { key = "K",        mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
     { key = "L",        mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
-    -- scroll setting
     { key = "PageUp",   mods = "LEADER",       action = act.ScrollByPage(-1) },
     { key = "PageDown", mods = "LEADER",       action = act.ScrollByPage(1) },
-    -- active command pallete
     { key = "P",        mods = "CTRL",         action = act.ActivateCommandPalette },
   },
 }
