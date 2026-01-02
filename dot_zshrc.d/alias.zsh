7079f185cc8facaa7f89e5449afef150028984ec
onikun94 <onikun94@gmail.com>
2026-01-02 10:12:49 +0900

    security: update home

--- dot_zshrc.d/alias.zsh
+++ dot_zshrc.d/alias.zsh
@@ -4,6 +4,8 @@ alias cdd='cd ~/Documents'
 alias cdh='cd ~'
 #cd previous
 alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
 #フォルダ作成
 alias m='mkdir'
 #削除(ファイル)
@@ -13,6 +15,9 @@ alias cl='clear'
 #ls
 alias ls='eza'
 alias ll='eza -l'
+alias la='eza -a'
+alias lf='eza --only-files'
+alias ld='eza --only-dirs'
 
 #zshrc
 alias zss='source ~/.zshrc'
@@ -23,3 +28,12 @@ alias grep='rg'
 #diff to delta
 alias diff='delta'
 
+# claude
+alias cc='claude'
+alias ccs='claude --dangerously-skip-permissions'
+
+# tmux
+alias tmux-panes="tmux split-window -h \\; split-window -v \\; select-pane -t 0 \\; split-window -v \\; select-pane -t 2 \\; split-window -v \\; select-pane -t 4 \\; split-window -v \\; select-pane -t 0"
+alias tmux-list="tmux list-panes -F \"#{pane_index}: #{pane_id} #{pane_current_command} #{pane_active}\""
+
+# Local aliases (machine-specific) are in ~/.zshrc.d/alias.local.zsh
--- dot_zshrc.d/path.zsh
+++ dot_zshrc.d/path.zsh
@@ -1,27 +1,22 @@
-#BUN
+# BUN
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
+[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
 
-#POETRY
+# POETRY
 export PATH="$HOME/.local/bin:$PATH"
 
-# bun completions
-[ -s "/Users/s19306/.bun/_bun" ] && source "/Users/s19306/.bun/_bun"
-
-# bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
-
-### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-export PATH="/Users/s19306/.rd/bin:$PATH"
-### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
-#
-#mise
+# mise
 eval "$(~/.local/bin/mise activate zsh)"
 
-#go
+# go
 export PATH=$PATH:$HOME/go/bin
 
-#nvm
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+# MySQL
+export PATH="/opt/homebrew/opt/mysql@8.0/bin:$PATH"
+
+# tmux pbw
+source ~/.config/tmux/president-boss-workers/pbw-aliases.zsh
+
+# nodebrew (disabled)
+# export PATH=$HOME/.nodebrew/current/bin:$PATH
