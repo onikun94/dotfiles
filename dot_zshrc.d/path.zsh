a7ce805be630f64056d58c65e3168178151f0181
onikun94 <onikun94@gmail.com>
2026-09-23 10:55:48 +0900

    chore: remove unused Conda, MySQL, and GCP settings

--- dot_zshrc.d/path.zsh
+++ dot_zshrc.d/path.zsh
@@ -19,12 +19,6 @@ fi
 # Go user binaries
 export PATH="$HOME/go/bin:$PATH"
 
-# MySQL is optional; do not add a dead path when it is not installed.
-MYSQL_BIN="/opt/homebrew/opt/mysql@8.0/bin"
-if [[ -d "$MYSQL_BIN" ]]; then
-  export PATH="$MYSQL_BIN:$PATH"
-fi
-
 # nodebrew (disabled)
 # export PATH=$HOME/.nodebrew/current/bin:$PATH
 
@@ -34,10 +28,5 @@ fi
 # meet-transcribe
 export PATH="$HOME/bin:$PATH"
 
-# Remove stale inherited entries after all optional PATH contributors ran.
+# Avoid duplicate PATH entries.
 typeset -U path
-for stale_path in \
-  "/opt/homebrew/opt/mysql@8.0/bin" \
-  "/opt/homebrew/Caskroom/miniforge/base/bin"; do
-  [[ -d "$stale_path" ]] || path=(${path:#$stale_path})
-done
